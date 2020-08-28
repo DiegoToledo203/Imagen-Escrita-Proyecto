991f1b48f1a01fd0fd7c04483f28c600e9c413be
DiegoToledo203 <diego.toledo.o@mail.pucv.cl>
2020-08-28 15:33:22 -0400

    Intento 1.2

---
+++ sketch.js
@@ -0,0 +1,16 @@
+
+let sketch;
+
+function setup() {
+  sketch = createCanvas(700, 400);
+  sketch.parent("p5");
+}
+
+function draw() {
+
+  for(let i = 0; i < 1000; i++){
+  stroke(random(295), random(205), random(375));
+  strokeWeight(random(2, 10));
+  point(random(width), random(height));
+  }
+}
